daa4fb11a4089883461143093f383dccaf2a35d9
tylersavery <tyler@dinosaur-factory.com>
2012-03-06 15:36:51 -0500

    frontent

--- private/packages/site/views/site.php
+++ private/packages/site/views/site.php
@@ -1,3 +1,99 @@
 {{{view.site_header}}}
-hellooo
+
+<div class="wrap">
+    <div id="header">
+        <div id="logo">Get Fresh Company</div>
+        <div id="menu">
+            <ul>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#events">Events</a></li>
+                <li><a href="#store">Store</a></li>
+                <li><a href="#kinlock">Kinlock</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div><!-- /#menu -->
+    </div><!-- /#header -->
+    
+    <!-- About Us -->
+    <div class="container" id="container_about">
+        <div class="content" id="content_about">
+            <div class="content_title">About Us</div>
+            <div class="content_copy">
+                <p>Get fresh company. We take pride in our business. Our priority is to keep our clientele fresh. Since our launch into the fashion world in 2009, we have been and continue to be purveyors of freshness and style through our online and Toronto-based shop. We keep our customers happy after every purchase because we integrate them into the get fresh lifestyle; unparelleled confidence with the style to match everyday.</p>
+            </div><!-- /.content_copy -->
+        </div><!-- /#content_about -->
+    </div><!-- /#container_about -->
+    
+    
+    <!-- Events -->
+    <div class="container" id="container_events">
+        <div class="content" id="content_events">
+            <div class="content_title">Events</div>
+            <div class="content_copy">
+                <p>We create events surrounding the Get Fresh culture. An ambience for those who like to stunt and who they are. A person not afraid to express how they feel with their style because it's a true expression of what they represent.
+Our events are designed to showcase the brands we sell, and introduce their philosophy to our customers. We know that the only brands that thrive are the ones that don't just make clothes they influence our lifestyles as well. With our pop-up shops we purvey this and more.</p>
+            </div><!-- /.content_copy -->
+        </div><!-- /#content_events -->
+    </div><!-- /#container_events -->
+    
+    
+    <!-- Store -->
+    <div class="container" id="container_store">
+        <div class="content" id="content_store">
+            <div class="content_title">Store</div>
+            <div class="content_copy">
+                <p>To look fresh, you have to be fresh.</p>
+                <p>The Get Fresh Company online store is designed to bring originality back to the shopping experience. Selling confidence and bold designs and a variation of stylistic influences.  All garments are hand-selected by in-house fashion stylist "FRESH". You will find nothing but the latest so enjoy!</p>
+            </div><!-- /.content_copy -->
+        </div><!-- /#content_store -->
+    </div><!-- /#container_store -->
+
+
+    <!-- Kinlock -->
+    <div class="container" id="container_kinlock">
+        <div class="content" id="content_kinlock">
+            <div class="content_title">Kinlock Collection</div>
+            <div class="content_copy">
+                <p>Kinlock Collection, the brand will lock you in at first sight.  The accessory that compliments an outfit, or an accessory that can be the main part of your outfit. Made in Dubai, UAE and designed by Toronto stylist, Fresh, The Kinlock Collection was created in 2010, in honour of his mother's flamboyant yet classy style.</p>
+            </div><!-- /.content_copy -->
+        </div><!-- /#content_kinlock -->
+    </div><!-- /#container_kinlock -->
+
+    <!-- Blog -->
+    <div class="container" id="container_blog">
+        <div class="content" id="content_blog">
+            <div class="content_title">Blog</div>
+            <div class="content_copy">
+                <p>Always know whats next</p>
+            </div><!-- /.content_copy -->
+        </div><!-- /#content_blog -->
+    </div><!-- /#container_blog -->
+
+
+    <!-- Contact -->
+    <div class="container" id="container_contact">
+        <div class="content" id="content_contact">
+            <div class="content_copy">
+                <h3>Get Fresh Company</h3>
+                <div id="contact_address">
+                    498 Queens st W<br /> 
+                    Toronto, Ontario<br />
+                    M5V 2B3<br />
+                    Canada
+                </div><!-- /#contact_address -->
+                <div id="contact_details">
+                    <span>Phone:</span>647 746 3669<br />
+                    <span>Email:</span><a href="mailto:info@getfreshcompany.com">info@getfreshcompany.com</a><br />
+                    <span>Twitter:</span>@freshboss<br />
+                    <span>Facebook:</span><a href="#">Get Fresh</a><br />
+                    <span>Instagram:</span><a href="#">Freshboss</a><br />
+                </div>
+            </div><!-- /.content_copy -->
+        </div><!-- /#content_contact -->
+    </div><!-- /#container_contact -->
+
+
+</div><!-- /.wrap -->
+
 {{{view.site_footer}}}
