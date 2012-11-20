8aff12c4dcecd1140c3e133cba49a96a9cab59fd
tylersavery <tyler@theyoungastronauts.com>
2012-11-20 13:14:03 -0500

    quick update

--- private/packages/site/views/site.php
+++ private/packages/site/views/site.php
@@ -103,7 +103,8 @@
             <div class="content_copy">
             
             <p>The Get Fresh Company collaborative store and office is located at 498 Queen Street West, Toronto, Canada.</p>
-            <P> STORE HOURS<br />
+            <p></p>
+            <P style="text-alight:right;"> STORE HOURS<br />
 
                 MONDAY - THURSDAY [12PM-7PM]<br />
 
